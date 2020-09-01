e2190cd51814291ece8aab50704533fcff793bd3
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-09-01 22:01:26 +0545

    models and admin

--- site/src/personal/admin.py
+++ site/src/personal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from personal.models import Question
 
 # Register your models here.
+
+admin.site.register(Question)
+
--- site/src/personal/views.py
+++ site/src/personal/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
+from personal.models import Question
 
 def render_home_view(request):
 
     context = {}
-    
-    list_of_values = []
-    list_of_values.append("First Number")
-    list_of_values.append("second Number")
-    list_of_values.append("third Number")
-    list_of_values.append("Fourth Number")
-    list_of_values.append("Fifth Number")
 
-    context["list_of_values"] = list_of_values
+    questions = Question.objects.all()
+    context["questions"] = questions
 
     return render(request, "personal/index.html", context)
