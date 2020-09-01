6e551f582451885e43a3d96fc886ed550e7ef0c3
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-09-01 17:59:58 +0545

    variables to templates

--- site/src/personal/views.py
+++ site/src/personal/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
 def render_home_view(request):
-    print(request.headers)
-    return render(request, "base.html", {})
+
+    context = {}
+    
+    list_of_values = []
+    list_of_values.append("First Number")
+    list_of_values.append("second Number")
+    list_of_values.append("third Number")
+    list_of_values.append("Fourth Number")
+    list_of_values.append("Fifth Number")
+
+    context["list_of_values"] = list_of_values
+
+    return render(request, "personal/index.html", context)
