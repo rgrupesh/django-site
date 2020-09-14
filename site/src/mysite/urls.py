28b8b2fff4f1d88edff105d91fa7008205cd4b50
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-09-14 12:46:05 +0545

    static files

--- site/src/mysite/urls.py
+++ site/src/mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 from personal.views import render_home_view
@@ -50,3 +52,7 @@ urlpatterns = [
      name='password_reset_complete'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
