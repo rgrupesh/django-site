472e79ce7179b70db7a561c5d7bad94600ac2efb
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-09-17 12:13:28 +0545

    django REST

--- site/src/blog/models.py
+++ site/src/blog/models.py
@@ -11,7 +11,7 @@ def upload_location(instance, filename, **kwargs):
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
-    body = models.TextField(max_length=50, null=False, blank=False)
+    body = models.TextField(max_length=2500, null=False, blank=False)
     image = models.ImageField(upload_to= upload_location, null=False, blank=False)
     date_published = models.DateTimeField(auto_now_add=True, verbose_name="date_published")
     date_updated = models.DateTimeField(auto_now_add=True, verbose_name="date_updated")
