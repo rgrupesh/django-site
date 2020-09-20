927f76d43b1b902107b3514e2cf410ce54436037
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-09-20 09:24:04 +0545

    PUT and DELETE

--- site/src/blog/api/views.py
+++ site/src/blog/api/views.py
@@ -15,4 +15,41 @@ def api_detail_blog_view(request,slug):
         return Response(status=status.HTTP_404_NOT_FOUND)    
 
     serializer= BlogPostSerializers(blog_post)
-    return Response(serializer.data)    
+    return Response(serializer.data)
+
+
+@api_view(['PUT',])
+def api_update_blog_view(request,slug):
+
+    try:
+        blog_post = BlogPost.objects.get(slug=slug)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = BlogPostSerializers(blog_post, data=request.data)
+    data ={}
+    if serializer.is_valid():
+        serializer.save()
+        data['success'] = 'updated successfully'
+        return Response(data=data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE',])
+def api_delete_blog_view(request,slug):
+
+    try:
+        blog_post = BlogPost.objects.get(slug=slug)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)            
+
+    operation = blog_post.delete()
+    if operation:
+        data['success'] = 'deleted successfully'
+        return Response(data=data)
+    else:
+        data['failure'] = 'delete failed'
+        return Response(data=data)
+
+
+
