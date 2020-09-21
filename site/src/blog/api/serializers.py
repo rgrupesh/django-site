bf451f0cdcacfc01758a1000bd678d7eb4452153
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-09-21 15:52:25 +0545

    SerializersMethodField to add additional field

--- site/src/blog/api/serializers.py
+++ site/src/blog/api/serializers.py
@@ -4,6 +4,13 @@ from blog.models import BlogPost
 
 
 class BlogPostSerializers(serializers.ModelSerializer):
+
+    username = serializers.SerializerMethodField('get_username')
+
     class Meta:
         model = BlogPost
-        fields = ['title','body','image','date_updated']
+        fields = ['title','body','image','date_updated','username']
+
+    def get_username(self, blog_post):
+        username = blog_post.author.username
+        return username     
